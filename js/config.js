453b5eac15f937687f1aef793f9be361f92e5c1d
James Dsouza <jamesdsouza03111999@gmail.com>
2026-08-28 18:39:46 +0530

    28-08-2026

--- js/config.js
+++ js/config.js
@@ -1,10 +1,9 @@
 /* =========================================================
-   WEBSITE CONFIGURATION
-   Change paths here only.
+   EXTERNAL LINK CONFIGURATION
+   Open external destinations in a new browser tab.
 ========================================================= */
-const CONFIG = {
-    RESUME: "assets/documents/Resume May 2026.pdf",
-    LINKEDIN: "https://www.linkedin.com/in/james-dsouza-9baa1b212/",
-    GITHUB: "https://github.com/notjamesdsouza",
-    EMAIL: "mailto:jamesdsouza03111999@gmail.com"
-};
+
+const resumeLink = document.getElementById("https://drive.google.com/file/d/1wcJGUSCHFR8AWqRvehJRoLpFXJ1wqgF0/view?usp=drive_link");
+const linkedinLink = document.getElementById("https://www.linkedin.com/in/james-dsouza-9baa1b212/");
+const githubLink = document.getElementById("https://github.com/notjamesdsouza");
+const emailLink = document.getElementById("mailto:jamesdsouza03111999@gmail.com");
